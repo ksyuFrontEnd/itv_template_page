abf15e94bda2c5b6d4ace183bf6b9400daaee9ab
Olga Barnikova <barnikovaolga8@gmail.com>
2023-08-30 16:10:45 +0300

    added header

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,3 +1,13 @@
+let line = document.querySelector('.icon-menu');
+let links = document.querySelectorAll('.menu__link');
+let menu = document.querySelector('.menu__body');
+let overlay = document.querySelector('.burger-menu__overlay');
 
+line.addEventListener('click', toogleMenu);
+overlay.addEventListener('click', toogleMenu);
 
-console.log("header");
+function toogleMenu() {
+  line.classList.toggle('active');  
+  menu.classList.toggle('active');
+  document.body.classList.toggle('lock');   
+}
