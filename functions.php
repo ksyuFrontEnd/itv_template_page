a3b3f6503b2810ee63b8bf9ecf59ec8b376d1592
Iryna <Iryna@MacBook-Air-MacBook.local>
2023-10-05 13:02:30 +0300

    add temp parts

--- functions.php
+++ functions.php
@@ -43,6 +43,16 @@ function wp_it_volunteers_scripts() {
     wp_enqueue_style( 'projects-style', get_template_directory_uri() . '/assets/styles/template-styles/projects.css', array('main') );
     wp_enqueue_script( 'projects-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/projects.js', array(), false, true );
   }
+
+  if (is_singular() && locate_template('template-parts/join.php')) {
+    wp_enqueue_style( 'join-style', get_template_directory_uri() . '/assets/styles/template-parts-styles/join.css', array('main') );
+    wp_enqueue_script( 'join-scripts', get_template_directory_uri() . '/assets/scripts/template-parts-scripts/join.js', array(), false, true );
+    }
+
+    if (is_singular() && locate_template('template-parts/form.php')) {
+    wp_enqueue_style( 'form-style', get_template_directory_uri() . '/assets/styles/template-parts-styles/form.css', array('main') );
+    wp_enqueue_script( 'form-scripts', get_template_directory_uri() . '/assets/scripts/template-parts-scripts/form.js', array(), false, true );
+    }
 }
 /** add fonts */
 function add_google_fonts() {
