f542f14c64684bafa15035dfc0a3994910508a6a
Olga Barnikova <barnikovaolga8@gmail.com>
2023-10-26 15:32:40 +0300

    fixed button in header to form

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -2,9 +2,11 @@ let line = document.querySelector('.icon-menu');
 let links = document.querySelectorAll('.menu__link');
 let menu = document.querySelector('.menu__body');
 let overlay = document.querySelector('.burger-menu__overlay');
+let menuButton = document.querySelector('.menu__btn');
 
 line.addEventListener('click', toogleMenu);
 overlay.addEventListener('click', toogleMenu);
+menuButton.addEventListener('click', toogleMenu);
 
 function toogleMenu() {
   line.classList.toggle('active');  
