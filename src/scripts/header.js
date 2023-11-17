4533b550e4d09b37c4ce3b2a06f51dd3f2a7087b
Olga Barnikova <barnikovaolga8@gmail.com>
2023-11-17 12:30:10 +0200

    fixed bags

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,14 +1,15 @@
 let line = document.querySelector('.icon-menu');
-let links = document.querySelectorAll('.menu__link');
+let links = document.querySelectorAll('.menu-item');
 let menu = document.querySelector('.menu__body');
 let overlay = document.querySelector('.burger-menu__overlay');
 let menuButton = document.querySelector('.menu__btn');
-let menuLink = document.getElementById('menu-item-418');
 
 line.addEventListener('click', toogleMenu);
 overlay.addEventListener('click', toogleMenu);
 menuButton.addEventListener('click', toogleMenu);
-menuLink.addEventListener('click', toogleMenu);
+for (let link of links) { 
+  link.addEventListener('click', toogleMenu);
+}
 
 function toogleMenu() {
   line.classList.toggle('active');  
